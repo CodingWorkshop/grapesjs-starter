6ec274dca80f03b363e05cbaf253909e6c7306c2
Smile <cxz0917001997@gmail.com>
2019-08-09 13:26:12 +0800

    add extra blocks

--- src/main.js
+++ src/main.js
@@ -1,12 +1,42 @@
 import grapesjs from 'grapesjs';
-import grapesjsPresetWebpage from 'grapesjs-preset-webpage';
 import './style.css';
+import 'grapesjs-preset-webpage';
+import 'grapesjs-lory-slider';
+import 'grapesjs-tabs';
+import pluginCustomCode from 'grapesjs-custom-code';
+import pluginTooltip from 'grapesjs-tooltip';
 
 var editor = grapesjs.init({
   container: '#gjs',
   components: '<div class="txt-red">Hello world!</div>',
   style: '.txt-red{color: red}',
   plugins: [
-    'gjs-preset-webpage'
-  ]
+    'gjs-preset-webpage',
+    'grapesjs-lory-slider',
+    'grapesjs-tabs',
+     pluginCustomCode,
+     pluginTooltip
+  ],
+  pluginsOpts: {
+    'grapesjs-lory-slider': {
+      sliderBlock: {
+        category: 'Extra'
+      }
+    },
+    'grapesjs-tabs': {
+      tabsBlock: {
+        category: 'Extra'
+      }
+    },
+    [pluginCustomCode]: {
+      blockCustomCode: {
+        category: 'Extra'
+      }
+    },
+    [pluginTooltip]: {
+      blockTooltip: {
+        category: 'Extra'
+      }
+    }
+  }
 });
