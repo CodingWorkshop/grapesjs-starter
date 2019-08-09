eebf78d355c0714f4f1ceb9fd1c41e6a53380200
Smile <cxz0917001997@gmail.com>
2019-08-09 10:42:02 +0800

    grapesjs editor init

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import grapesjs from 'grapesjs';
+import grapesjsPresetWebpage from 'grapesjs-preset-webpage';
 import './style.css';
 
 var editor = grapesjs.init({
   container: '#gjs',
   components: '<div class="txt-red">Hello world!</div>',
   style: '.txt-red{color: red}',
-  plugins: ['gjs-preset-webpage'],
-  pluginsOpts: {
-    'gjs-preset-webpage': {
-    }
-  }
+  plugins: [
+    'gjs-preset-webpage'
+  ]
 });
